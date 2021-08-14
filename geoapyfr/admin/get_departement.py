67cbb700c75ec7ce703a0f0dfed531e50ecf4505
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-08-14 10:53:46 +0200

    add departement argument

--- geoapyfr/admin/get_departement.py
+++ geoapyfr/admin/get_departement.py
@@ -6,10 +6,12 @@ from shapely.ops import cascaded_union
 from geoapyfr.admin.get_commune import get_commune
 from geoapyfr.admin._get_departement_list import _get_departement_list
 
-def get_departement(region=None, geometry=False, geo='france-zoom-overseas-paris'):
+def get_departement(region=None, departement=None,
+                    geometry=False, geo='france-zoom-overseas-paris'):
     
     deps = _get_departement_list(region=region)
     deps.columns = list_col = ['departement_nom', 'departement_code', 'region_code']
+    
     
     if geometry is True:
         communes = get_commune(region=region,
@@ -18,6 +20,9 @@ def get_departement(region=None, geometry=False, geo='france-zoom-overseas-paris
         
         dep_list = communes.departement_code.unique()
         
+        if departement is not None:
+            dep_list = [dep for dep in dep_list if dep in departement]
+            
         list_dep_with_geom = []
         
         for d in dep_list:
@@ -35,7 +40,10 @@ def get_departement(region=None, geometry=False, geo='france-zoom-overseas-paris
             df['surface'] = surface
             list_dep_with_geom.append(df)
         
-        deps = pd.concat(list_dep_with_geom)   
+        deps = pd.concat(list_dep_with_geom)  
+    
+    if departement is not None:
+        deps = deps[deps['departement_code'].isin(departement)]
         
     deps = deps.reset_index(drop=True)
     
